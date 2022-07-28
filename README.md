7973bc484beb0ca70db3a5b8561c9af146069a79
jah377 <jonathan.harris.eit@gmail.com>
2022-07-28 14:59:43 +0200

    clean up main

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# FoDSD4
+# Fundamentals of Data Science Course (5294FUDS6Y): United Nations General Assembly NLP Project
 
-Assignment 1 
+![ScreenShot](https://raw.github.com/jah377/NLP_GeneralAssembly/main/report/github_wordcloud.png)
+
+This repository contains code corresponding to the analyses presented in the final paper for the Fundamentals of Data Science course, in partial fulfillment for the degree of MSc. in Data Science at the University of Amsterdam.
+
+## Executive Summary
+
+The United Nations General Assembly offers a unique context to study international politics, as the speeches represent its member states official perception of and stance on current political developments. In this paper, we explore if differences in development strata, based on the Human Development Index (HDI), are reflected in the General Assembly speeches. Exploratory data analyses aimed to identify differences in word usage and frequency, speech sentiment, class-to-class reference patterns and investigate the feasibility of a multinomial classification algorithm. Our results show minimal differences in word usage between the development classes, with cosine similarities ranging from (0.93-0.99). Moreover, we find no relationship between HDI score and speech sentiment and that the proportion of references to countries in the four classes are similar across all groups. Finally, we implemented a logistic regression model with an accuracy of 71%, thus indicating some correlation between HDI class and word usage in speeches. However, the top 5 words are very general and don’t relate to overarching topics or themes. *In toto*, we did not find evidence that there is no relationship between the level of development and the content of the corresponding speeches during the General Assembly.
+
+The submitted manuscript can be found within the repo [link](https://raw.github.com/jah377/NLP_GeneralAssembly/main/report/manuscript.pdf)
--- util.py
+++ util.py
@@ -1,13 +1,71 @@
 import re
-import nltk
 import numpy as np
 import pandas as pd
+
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from text2digits import text2digits
+
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score, precision_recall_fscore_support
-from nltk.stem import WordNetLemmatizer
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+
+
 nltk.download('wordnet')
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('wordnet')
+set(stopwords.words('english'))
+
+
+def clean_string(s):
+    """
+    remove defined characters and substrings
+    """
+    t2d = text2digits.Text2Digits()  # instantiate model
+
+    s = s.lower()  # normalize lower case
+    s = re.sub('mr\.', ' ', s)  # remove Mr.
+    s = re.sub('mrs\.', ' ', s)  # remove Mrs.
+    s = re.sub('[\(\[].*?[\)\]]', '', s)  # remove references
+    s = re.sub('[0-9]+.\t', '', s)  # remove paragraph numbers
+    s = re.sub('\n ', ' ', s)  # remove new lines
+    s = re.sub('\n', ' ', s)  # remove new lines
+    s = re.sub("'s", ' ', s)  # remove apostrophe-s
+    s = re.sub("'", '', s)  # remove apostrophe
+    s = re.sub('-', ' ', s)  # remove hyphens
+    s = re.sub('- ', ' ', s)  # remove hyphens
+    s = re.sub("\'", '', s)  # remove quotations
+
+    s = re.sub(r'[^a-zA-Z0-9 ]', '', s)  # remove punctuation/non-alpha
+    s = t2d.convert(s)  # converts 'fifth' to 5 -- remove later
+    s = re.sub(r'[0-9]', ' ', s)  # remove numbers from t2d
+    s = re.sub(r'  +', ' ', s)  # remove 2+ spaces
+    s = s.strip()  # remove leading-trailing spaces
+
+    return s
+
+
+def remove_stopwords(tokens):
+    """
+    remove stopwords and short words from clean, tokenized string
+    """
+    n_char = 3  # remove short words (arbitrary)
+    sw = stopwords.words("english")  # stop words
+    sw += ['assembly', 'community', 'continue', 'countries', 'country', 'general',
+           'government', 'human', 'international', 'like', 'make', 'many', 'member',
+           'must', 'nations', 'national', 'need', 'organization', 'organisation',
+           'people', 'president', 'process', 'republic', 'region', 'secretary', 'session', 'state',
+           'states', 'time', 'united', 'well', 'work', 'world', 'year']  # stop words determined from wordcloud
+    sw = [*map(clean_string, sw)]  # reformat to match cleaned text
+
+    # remove useless or short words
+    tokens_wo_sw = [word for word in tokens
+                    if (word not in sw) and len(word) > n_char]
+
+    return tokens_wo_sw
 
 
 def remove_names(text: str):
@@ -49,7 +107,8 @@ def remove_single_occurence(speeches):
         single_word = vocabulary[int(single_index)]
         single_words.append(single_word)
 
-    speeches = speeches.apply(lambda x: [w for w in x if w not in single_words])
+    speeches = speeches.apply(
+        lambda x: [w for w in x if w not in single_words])
 
     return speeches
 
@@ -70,7 +129,8 @@ def logreg(X, y):
     create a logistic regression model for classification
     """
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42)
     model = LogisticRegression().fit(X_train, y_train)
     pred = model.predict(X_test)
     classes = model.classes_
@@ -97,7 +157,7 @@ def top_n_distinctive_words(n, tf_idf, classes, weights):
         words = []
 
         for index in max_indices:
-            max_feature = tf_idf.columns[index -1]
+            max_feature = tf_idf.columns[index - 1]
             words.append(max_feature)
 
         class_words_dict[classname] = words
@@ -112,6 +172,5 @@ def display_words(words_dict: dict):
 
     for key in words_dict:
         print(key)
-        print(*words_dict[key], sep = ", ") 
+        print(*words_dict[key], sep=", ")
         print('\n')
-        
